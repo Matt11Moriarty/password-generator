202904db8bd2c5443e31975ea2c1d73c360dd105
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-06-27 18:25:00 -0700

    added arrays

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # password-generator
+
+Resources:
+
+- shuffle an array 
+  - https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
+
+- create a string from an array
+  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+- random gen resources
+  - https://www.slingacademy.com/article/ways-to-generate-random-strings-in-javascript/
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//arrays
+lowerCase    = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+upperCase    = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+numbers      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+specialChars = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+
+console.log(specialChars);
+
+for (i=0; i<specialChars.length; i++) {
+  console.log(specialChars[i]);
+}
