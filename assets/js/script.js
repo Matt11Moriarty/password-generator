6719e488e642e2c29327cc8f5e82e8daa51d3717
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-06-28 09:07:30 -0700

    got getPasswordLength working

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,24 +17,28 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   //arrays
-  lowerCase    = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  upperCase    = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  numbers      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  specialChars = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+  var lowerCase    = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase    = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numbers      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialChars = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 
   //gets user input for the desired length of the password. Only takes numbers between a range
   function getPasswordLength() {
     var pwdLength = window.prompt("How long would you like your password to be? Must be a number between 8 and 128.");
-    if (pwdLength < 8 || pwdLength > 128 || typeof pwdLength !== Number) {
+    if (pwdLength < 8 || pwdLength > 128 || isNaN(pwdLength)) {
       window.alert("Can't you read?");
-      setTimeout(function(){
-        generatePassword();
-      }, 1000); 
+        getPasswordLength();
+
+    } else {
+      return parseInt(pwdLength);
     }
   }
-  getPasswordLength();
-    
+  //store the value of getPasswordLength()
+  var chosenLength = getPasswordLength();
+  console.log(chosenLength);
+ 
 }
 
 
 generatePassword();
+
