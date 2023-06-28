90b7a4506f15ec8c31cfa0c3b01af4c18f47af8e
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-06-27 19:30:34 -0700

    moved arerays and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,15 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//arrays
-lowerCase    = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-upperCase    = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-numbers      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-specialChars = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 
 function generatePassword() {
-  
+  //arrays
+  lowerCase    = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  upperCase    = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  numbers      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  specialChars = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+
+  //gets user input for the desired length of the password. Only takes numbers between a range
   function getPasswordLength() {
     var pwdLength = window.prompt("How long would you like your password to be? Must be a number between 8 and 128.");
     if (pwdLength < 8 || pwdLength > 128 || typeof pwdLength !== Number) {
